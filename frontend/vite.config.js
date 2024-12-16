45cb6f4f55de98f2f64598b608dd32ab06a15fc7
1cywind <nguyenkhoi200455@gmail.com>
2024-12-16 12:05:40 +0700

    Fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
     plugins: [react()],
-=======
-    plugins: [react()]
->>>>>>> dca16b2581e7ac8afeec84b8097cc7a0b7aa2110
 })
